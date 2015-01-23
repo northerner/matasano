1af685c71b8d2db3b7e10ef90790e1abc872f35b
Daniel King <dan@llamadigital.net>
2015-01-23 22:04:28 +0000

    first attempt at set 1 task 2

--- lib/matasano/set_1.ex
+++ lib/matasano/set_1.ex
@@ -1,7 +1,42 @@
 defmodule Matasano.Set1 do
+  use Bitwise
+
   def hex_to_base64(hex_string) do
     String.upcase(hex_string)
     |> Base.decode16!
     |> Base.encode64
+  end
+
+  def fixed_xor(left, right) do
+    # [b_left, b_right] = for hex_string <- [left, right] do
+    #   String.upcase(hex_string)
+    #   |> Base.decode16!
+    #   |> Enum.map fn(char) -> <<char::16>> end
+    #   |> IO.puts
+    # end
+    # Bitwise.bxor(b_left, b_right)
+  end
+
+  defp hex_to_binary_array(hex_string) do
+    Base.decode16! hex_string
+  end
+
+  def dec_to_binary(1), do: [1] 
+  def dec_to_binary(dec), do: dec_to_binary(div(dec, 2), [rem(dec, 2)])
+  defp dec_to_binary(1, list), do: [1 | list] 
+  defp dec_to_binary(dec, list), do: dec_to_binary(div(dec, 2), [rem(dec, 2) | list])
+
+  def xor_binary_lists(binary_a, binary_b) do
+    zip = Enum.zip(binary_a, binary_b)
+    for {a, b} <- zip, do: Bitwise.bxor(a, b)
+  end
+
+  def binary_to_dec(binary_list) do
+    Enum.reverse(binary_list)
+    |> binary_to_dec(1, 0)
+  end
+  defp binary_to_dec([], _exponent, acc), do: acc
+  defp binary_to_dec([h|t], exponent, acc) do
+    binary_to_dec(t, exponent * 2, acc + (h * exponent))
   end
 end
--- test/matasano/set_1_test.exs
+++ test/matasano/set_1_test.exs
@@ -6,4 +6,11 @@ defmodule MatasanoSet1Test do
       "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") ==
       "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
   end
+
+  test "fixed xor" do
+    assert Matasano.Set1.fixed_xor(
+      "1c0111001f010100061a024b53535009181c",
+      "686974207468652062756c6c277320657965") ==
+      "746865206b696420646f6e277420706c6179"
+  end
 end
