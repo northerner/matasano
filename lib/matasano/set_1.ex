ba4256e696c4342f09f6dc90ef75829c8b45ac48
Daniel King <dan@llamadigital.net>
2015-01-24 00:44:09 +0000

    second attempt at set 1 task 2

--- lib/matasano/set_1.ex
+++ lib/matasano/set_1.ex
@@ -8,35 +8,26 @@ defmodule Matasano.Set1 do
   end
 
   def fixed_xor(left, right) do
-    # [b_left, b_right] = for hex_string <- [left, right] do
-    #   String.upcase(hex_string)
-    #   |> Base.decode16!
-    #   |> Enum.map fn(char) -> <<char::16>> end
-    #   |> IO.puts
-    # end
-    # Bitwise.bxor(b_left, b_right)
+    [result | _] = binary_xor(hex_to_binary_list(left), hex_to_binary_list(right))
+    Integer.to_string(result, 16)
   end
 
-  defp hex_to_binary_array(hex_string) do
-    Base.decode16! hex_string
+  def hex_to_binary_list(hex) do
+    String.upcase(hex)
+    |> String.to_integer(16)
+    |> Integer.to_string(2)
+    |> String.split
   end
 
-  def dec_to_binary(1), do: [1] 
-  def dec_to_binary(dec), do: dec_to_binary(div(dec, 2), [rem(dec, 2)])
-  defp dec_to_binary(1, list), do: [1 | list] 
-  defp dec_to_binary(dec, list), do: dec_to_binary(div(dec, 2), [rem(dec, 2) | list])
-
-  def xor_binary_lists(binary_a, binary_b) do
-    zip = Enum.zip(binary_a, binary_b)
-    for {a, b} <- zip, do: Bitwise.bxor(a, b)
+  def binary_xor(left, right) do
+    Enum.zip(left, right)
+    |> Enum.map(&(bxor_pair(&1)))
   end
 
-  def binary_to_dec(binary_list) do
-    Enum.reverse(binary_list)
-    |> binary_to_dec(1, 0)
+  defp bxor_pair({left, right}) do
+    lb = String.to_integer(left)
+    rb = String.to_integer(right)
+    Bitwise.bxor(lb, rb) 
   end
-  defp binary_to_dec([], _exponent, acc), do: acc
-  defp binary_to_dec([h|t], exponent, acc) do
-    binary_to_dec(t, exponent * 2, acc + (h * exponent))
-  end
+
 end
